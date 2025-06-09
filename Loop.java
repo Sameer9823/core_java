4a5c2e74930075ff9840976b7a437bd8f4b4ae13
Sameer Selokar <sameerselokar924@gmail.com>
2025-06-09 19:06:01 +0530

    Add Loop class demonstrating multiplication and even number identification

---
+++ Loop.java
@@ -0,0 +1,15 @@
+public class Loop {
+    public static void main(String[] args) {
+        int num = 2;
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(num + " * " + i + " = " + (num * i));        
+
+        }
+
+        for (int i = 0;i <=55; i++){
+            if(i % 2 ==0){
+                System.out.println(i + " is even");
+            } 
+        }
+    }
+}
