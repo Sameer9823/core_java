4307fa36f02392ca4dd812dd70d31246d7df1465
Sameer Selokar <sameerselokar924@gmail.com>
2025-06-09 14:24:55 +0530

    day 1

---
+++ hello.java
@@ -0,0 +1,7 @@
+
+class HelloWorld {
+    public static void main(String[] args) {
+    // This is a simple Java program that prints "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
---
+++ java.md
@@ -0,0 +1,10 @@
+what is java?
+what is jvm?
+WHAT IS JRE? WORA
+what is os?
+what is bytecode?
+how java execute the java code expalin the process with diagram?
+
+
+
+
