a67ab15ef6532a524c0c66f08991784954baac9e
Sameer Selokar <sameerselokar924@gmail.com>
2025-06-09 16:37:36 +0530

    Add Logical class demonstrating logical operations in Java

---
+++ Logical.java
@@ -0,0 +1,12 @@
+public class Logical {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 7;
+        int a = 3;
+        int b = 7;
+
+        // This is a simple Java program that demonstrates logical operations
+        boolean result1 = (x > y) && (a < b); // Logical AND
+        System.out.println(result1);
+    }
+}
