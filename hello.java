389f16d34e9ea92e363245883bc9b35977787466
Sameer Selokar <sameerselokar924@gmail.com>
2025-06-09 14:45:28 +0530

    Add HelloWorld and Variable classes with sum calculation

--- hello.java
+++ hello.java
@@ -1,7 +1,12 @@
 
+
 class HelloWorld {
     public static void main(String[] args) {
     // This is a simple Java program that prints "Hello, World!" to the console
+        int num1 = 3;
+        int num2 = 4;
+        int sum = num1 + num2;
+        System.out.println(sum);
         System.out.println("Hello, World!");
     }
 }
---
+++ variable.java
@@ -0,0 +1,11 @@
+
+
+class Variable {
+    public static void main(String args[]){
+        int num1 = 3;
+        int num2 = 4;
+        int sum = num1 + num2;
+        System.out.println(sum);
+        // This is a simple Java program that calculates the sum of two integers and prints the result
+    }
+}
