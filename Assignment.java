3dc3cbb14b96e8ff1640f6b3d3b4cb405553d8d9
Sameer Selokar <sameerselokar924@gmail.com>
2025-06-09 16:05:33 +0530

    Add Assignment class with main method for integer addition

---
+++ Assignment.java
@@ -0,0 +1,12 @@
+public class Assignment {
+
+    public static void main(String[] args) {
+        int num1 = 3; // Integer data type
+        int num2 = 4; // Integer data type
+
+        int sum = num1 + num2; // Adding two integers
+        System.out.println(sum);
+
+        
+    }
+}
