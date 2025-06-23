55d852e4db22d8010c70efa1dc8dbdb24108970c
Sameer Selokar <sameerselokar924@gmail.com>
2025-06-23 09:37:27 +0530

    Add whitespace for improved code readability in Demo class

--- Thread/Demo.java
+++ Thread/Demo.java
@@ -30,6 +30,7 @@ class B implements Runnable {
 public class Demo {
     public static void main(String[] args) {
         A obj1 = new A();
+        
         B obj2 = new B();
         
         // obj1.show();
