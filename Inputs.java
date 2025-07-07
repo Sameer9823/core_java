746565c34a4551ee6255885329ce72d587ce84c3
Sameer Selokar <sameerselokar924@gmail.com>
2025-07-07 18:19:06 +0530

    Add Inputs class to handle user input and output sum of two integers

---
+++ Inputs.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println(num + num2);
+    }
+}
